5ea2737a90e5b35d23fd3cbf23f05584849c0fd7
daiyuan <yn355555@dal.ca>
2022-03-31 16:31:04 -0300

    Front end UI

---
+++ front/pdf-slicer-ui/src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <button>Update Files</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:8000/getmissionid';
+      axios.get(path)
+        .then((res) => {
+          console.log(res);
+          this.msg = res.data['mission-id'];
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
